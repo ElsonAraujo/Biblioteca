e748d559ab683c3b29f899391ec1ff024bac79a6
ElsonAraujo <elsonro.araujo@hotmail.com>
2021-12-01 00:44:05 +0000

    Criei uma class EntidadeBase para gerar automaticamente o Id.

--- Models/Dtos/LivroDto.cs
+++ Models/Dtos/LivroDto.cs
@@ -1,18 +1,23 @@
-﻿using System;
+﻿using Biblioteca.Models.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Biblioteca.Models.Dtos
 {
-    public class LivroDto  // DataTransferObjects
+    public class LivroDto : EntidadeBase  // DataTransferObjects
     {
 
-        public string Id { get; set; }
+        // public string Id { get; set; } não mais necessário visto ter a classe id a gerar automaticamente 
         public string Nome { get; set; }
         public string Autor { get; set; }
         public string Editora { get; set; }
 
+        public LivroDto()
+        {
+            
+        }
         //construtores
         public LivroDto(string id, string nome, string autor, string editora)
             :this(nome, autor, editora)
